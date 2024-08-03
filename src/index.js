511bc72b9a7e6d9a3ae35a9621be071bd41af826
ElderDragonfly <ElderDragonfly@yandex.ru>
2024-08-03 13:45:56 +0300

    reverse int solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    let array = (Math.abs(n)).toString().split('');
+    let newArray = [];
+    console.log(array);
+    for(let i = array.length; i > 0; i--) {
+        newArray.push(array[i - 1]);
+    }
+    return newArray.join('');
+};
